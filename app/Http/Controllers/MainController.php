5b443886c7f777dbf1cd82411f535c0ae8ed3765
js3na <passosjoaogabriel29@gmail.com>
2025-01-21 17:48:38 -0300

    create: generate exercises function and views

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class MainController extends Controller
+{
+    public function home(): View
+    {
+        return view('home');
+    }
+    public function generateExercises(Request $request): View
+    {
+        $request->validate(
+            [
+                'check_sum' => 'required_without_all:check_subtraction,check_multiplication,check_division',
+                'check_subtraction' => 'required_without_all:check_sum,check_multiplication,check_division',
+                'check_multiplication' => 'required_without_all:check_sum,check_subtraction,check_division',
+                'check_division' => 'required_without_all:check_sum,check_multiplication,check_subtraction',
+
+                'number_one' => 'required|integer|min:0|max:999|lt:number_two',
+                'number_two' => 'required|integer|min:0|max:999',
+
+                'number_exercises' => 'required|integer|min:5|max:50',
+            ]
+        );
+
+        $operations = [];
+
+        $request->check_sum ?  $operations[] = 'sum' : null;
+        $request->check_subtraction ?  $operations[] = 'subtraction' : null;
+        $request->check_multiplication ?  $operations[] = 'multiplication' : null;
+        $request->check_division ?  $operations[] = 'division' : null;
+
+        $min = $request->number_one;
+        $max = $request->number_two;
+
+        $numberExercises = $request->number_exercises;
+
+        //generate exercises
+
+        $exercises = [];
+
+        for ($i = 1; $i <= $numberExercises; $i++) {
+            $exercises[] = $this->generateExercise($i, $operations, $min, $max);
+        }
+
+        session(['exercises' => $exercises]);
+
+        return view('operations', ['exercises' => $exercises]);
+    }
+    public function printExercises()
+    {
+        if(!session()->has('exercises')){
+            return redirect()->route('home');
+        }
+
+        $exercises = session('exercises');
+    }
+    public function exportExercises()
+    {
+        echo 'exportar exercicios para texto';
+    }
+
+    private function generateExercise($i, $operations, $min, $max): array
+    {
+        $operation = $operations[array_rand($operations)];
+        $number1 = rand($min, $max);
+        $number2 = rand($min, $max);
+
+        $exercise = '';
+        $solution = '';
+
+        switch ($operation) {
+            case 'sum':
+                $exercise = "$number1 + $number2 =";
+                $solution = $number1 + $number2;
+                break;
+            case 'subtraction':
+                $exercise = "$number1 - $number2 =";
+                $solution = $number1 - $number2;
+                break;
+            case 'multiplication':
+                $exercise = "$number1 x $number2 =";
+                $solution = $number1 * $number2;
+                break;
+            case 'division':
+
+                $number2 == 0 ? $number2 = 1 : null;
+
+                $exercise = "$number1 : $number2 =";
+                $solution = $number1 / $number2;
+                break;
+        }
+
+        if (is_float($solution)) {
+            $solution = round($solution, 2);
+        }
+
+        return [
+            'operation' => $operation,
+            'exercise_number' => $i,
+            'exercise' => $exercise,
+            'solution' => "$exercise $solution"
+        ];
+    }
+}
---
+++ resources/views/operations.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.main_layout')
+
+@section('content')
+
+<!-- operations -->
+<div class="container">
+
+    <hr>
+
+    <div class="row">
+
+        @foreach ($exercises as $exercise)
+
+        <div class="col-3 display-6 mb-3">
+            <span class="badge bg-dark">{{ str_pad($exercise['exercise_number'], 2, '0', STR_PAD_LEFT) }}</span>
+            <span>{{ $exercise['exercise'] }}</span>
+        </div>
+
+        @endforeach
+
+    </div>
+
+    <hr>
+
+</div>
+
+<!-- print version -->
+<div class="container mt-5">
+    <div class="row">
+        <div class="col">
+            <a href="{{ route('home') }}" class="btn btn-primary px-5">VOLTAR</a>
+        </div>
+        <div class="col text-end">
+            <a href="{{ route('exportExercises') }}" class="btn btn-secondary px-5">DESCARREGAR EXERCÍCIOS</a>
+            <a href="{{ route('printExercises') }}" class="btn btn-secondary px-5">IMPRIMIR EXERCÍCIOS</a>
+        </div>
+    </div>
+</div>
+
+@endsection
